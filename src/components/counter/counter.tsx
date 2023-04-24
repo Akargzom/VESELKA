ce7fbd255cebe3d72e58816b53237ac66e1a50c5
Alexander Ilyashenko <83316201+Akargzom@users.noreply.github.com>
2023-04-24 20:09:04 +0300

    Update counter.tsx

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -6,7 +6,7 @@ import vector from '../../assets/Vector.png'
 import { CounterProps} from "../../types/types"
 import API from "../../api/api"
 let Counter: React.FC<CounterProps> = (props) => {
-    moment.tz.add("Europe/Kiev|KMT EET MSK CEST CET MSD EEST|-22.4 -20 -30 -20 -10 -40 -30|0123434252525252525252525256161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161|-1Pc22.4 eUo2.4 rnz0 2Hg0 WM0 1fA0 da0 1v4m0 1db0 1cN0 1db0 1cN0 1db0 1dd0 1cO0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 Db0 3220 1cK0 1cL0 1cN0 1cL0 1cN0 1cL0 1cQ0 1cM0 1fA0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00|34e5")
+    moment.tz.add(props.timezone)
     const dateR = () => {
         let actualDate = ("0" + moment().tz("Europe/Kiev").date()).slice(-2) + "-" + ("0" + (moment().tz("Europe/Kiev").month() + 1)).slice(-2) + "-" + moment().tz("Europe/Kiev").year() + " " + ("0" + moment().tz("Europe/Kiev").hours()).slice(-2) + ":" + ("0" + moment().tz("Europe/Kiev").minutes()).slice(-2)
         return actualDate;
